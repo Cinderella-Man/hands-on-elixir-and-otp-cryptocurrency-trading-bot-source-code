b9445c6aafbc72a2bb1f2478063f564c22a420c0
Kamil Skowron <emanuel1989@gmail.com>
2021-02-06 00:29:03 +0000

    Update Decimal lib, convert trading settings to Decimal structs

--- apps/naive/lib/naive/trader.ex
+++ apps/naive/lib/naive/trader.ex
@@ -109,12 +109,11 @@ defmodule Naive.Trader do
   defp calculate_sell_price(buy_price, profit_interval, tick_size) do
     fee = D.new("1.001")
     original_price = D.mult(D.new(buy_price), fee)
-    tick_size = D.new(tick_size)
 
     net_target_price =
       D.mult(
         original_price,
-        D.add("1.0", D.from_float(profit_interval))
+        D.add("1.0", profit_interval)
       )
 
     gross_target_price = D.mult(net_target_price, fee)
@@ -135,5 +134,6 @@ defmodule Naive.Trader do
     |> Map.get("filters")
     |> Enum.find(&(&1["filterType"] == "PRICE_FILTER"))
     |> Map.get("tickSize")
+    |> Decimal.new()
   end
 end
