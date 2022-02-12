f16827cba5bfdba992b958fdfa2a53da4dbb363c
Kamil Skowron <emanuel1989@gmail.com>
2022-02-12 22:05:58 +0000

    Change get_env to compile_env for all env vars that will be assigned to attributes

--- mix.exs
+++ mix.exs
@@ -39,6 +39,8 @@ defmodule Hedgehog.MixProject do
   #
   # Run "mix help deps" for examples and options.
   defp deps do
-    []
+    [
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
+    ]
   end
 end
