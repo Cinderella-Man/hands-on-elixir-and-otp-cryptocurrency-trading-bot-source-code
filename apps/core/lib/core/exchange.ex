89b0a4df750dbf19d6b749317de28de0f24a230c
CinderellaMan <emanuel1989@gmail.com>
2022-08-08 14:15:52 +0100

    Exchange behaviour redefined and initial Binance implementation added

--- apps/core/lib/core/exchange.ex
+++ apps/core/lib/core/exchange.ex
@@ -1,30 +1,44 @@
 defmodule Core.Exchange do
-
   defmodule SymbolInfo do
     @type t :: %__MODULE__{
-      symbol: String.t(),
-      tick_size: number(),
-      step_size: number()
-    }
+            symbol: String.t(),
+            tick_size: number(),
+            step_size: number()
+          }
 
     defstruct [:symbol, :tick_size, :step_size]
   end
 
   defmodule Order do
     @type t :: %__MODULE__{
-      id: any(),
-      price: number(),
-      quantity: number(),
-      side: :buy | :sell,
-      status: :new | :filled,
-      timestamp: non_neg_integer()
-    }
+            id: any(),
+            price: number(),
+            quantity: number(),
+            side: :buy | :sell,
+            status: :new | :filled,
+            timestamp: non_neg_integer()
+          }
 
     defstruct [:id, :price, :quantity, :side, :status, :timestamp]
   end
 
-  @callback fetch_symbol_filters(symbol :: String.t()) :: Core.Exchange.SymbolInfo.t()
-  @callback get_order(symbol :: String.t(), timestamp :: non_neg_integer(), order_id :: non_neg_integer()) :: Core.Exchange.Order.t()
-  @callback order_limit_buy(symbol :: String.t(), quantity :: number(), price :: number()) :: Core.Exchange.Order.t()
-  @callback order_limit_sell(symbol :: String.t(), quantity :: number(), price :: number()) :: Core.Exchange.Order.t()
+  @callback fetch_symbols() ::
+              {:ok, [String.t()]}
+              | {:error, any()}
+  @callback fetch_symbol_filters(symbol :: String.t()) ::
+              {:ok, Core.Exchange.SymbolInfo.t()}
+              | {:error, any()}
+  @callback get_order(
+              symbol :: String.t(),
+              timestamp :: non_neg_integer(),
+              order_id :: non_neg_integer()
+            ) ::
+              {:ok, Core.Exchange.Order.t()}
+              | {:error, any()}
+  @callback order_limit_buy(symbol :: String.t(), quantity :: number(), price :: number()) ::
+              {:ok, Core.Exchange.Order.t()}
+              | {:error, any()}
+  @callback order_limit_sell(symbol :: String.t(), quantity :: number(), price :: number()) ::
+              {:ok, Core.Exchange.Order.t()}
+              | {:error, any()}
 end
