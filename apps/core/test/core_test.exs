c2a97080b8fd5526cb7dc629ca9664ce71059339
Kamil Skowron <emanuel1989@gmail.com>
2023-03-08 09:24:31 +0000

    Hello function's tests removed

--- apps/core/test/core_test.exs
+++ apps/core/test/core_test.exs
@@ -1,8 +1,4 @@
 defmodule CoreTest do
   use ExUnit.Case
   doctest Core
-
-  test "greets the world" do
-    assert Core.hello() == :world
-  end
 end
--- apps/indicator/test/indicator_test.exs
+++ apps/indicator/test/indicator_test.exs
@@ -1,8 +1,4 @@
 defmodule IndicatorTest do
   use ExUnit.Case
   doctest Indicator
-
-  test "greets the world" do
-    assert Indicator.hello() == :world
-  end
 end
