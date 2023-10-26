b12bc051a8b92055c2c4dc2e2e9f3d4c15690c85
Kamil Skowron <emanuel1989@gmail.com>
2023-10-26 12:40:58 +0200

    Logger.warn updates to Logger.warning

--- apps/naive/lib/naive/leader.ex
+++ apps/naive/lib/naive/leader.ex
@@ -60,7 +60,7 @@ defmodule Naive.Leader do
       ) do
     case Enum.find_index(traders, &(&1.pid == trader_pid)) do
       nil ->
-        Logger.warn("Tried to update the state of trader that leader is not aware of")
+        Logger.warning("Tried to update the state of trader that leader is not aware of")
         {:reply, :ok, state}
 
       index ->
@@ -79,7 +79,7 @@ defmodule Naive.Leader do
 
     case Enum.find_index(traders, &(&1.pid == trader_pid)) do
       nil ->
-        Logger.warn(
+        Logger.warning(
           "Tried to restart finished #{symbol} " <>
             "trader that leader is not aware of"
         )
@@ -102,7 +102,7 @@ defmodule Naive.Leader do
 
     case Enum.find_index(traders, &(&1.pid == trader_pid)) do
       nil ->
-        Logger.warn(
+        Logger.warning(
           "Tried to restart #{symbol} trader " <>
             "but failed to find its cached state"
         )
