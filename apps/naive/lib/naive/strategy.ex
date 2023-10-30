49f569b82bbe632a7a53d199f9cab4e4d3055463
Kamil Skowron <emanuel1989@gmail.com>
2023-10-30 15:00:14 +0100

    Strategy generate_decicion/3 clauses fixed

--- apps/naive/lib/naive/strategy.ex
+++ apps/naive/lib/naive/strategy.ex
@@ -119,8 +119,25 @@ defmodule Naive.Strategy do
         },
         _positions,
         _settings
-      ) do
+      )
+      when is_number(order_id) do
     :skip
+  end
+
+  def generate_decision(
+        %TradeEvent{
+          buyer_order_id: order_id
+        },
+        %Position{
+          buy_order: %Exchange.Order{
+            id: order_id
+          }
+        },
+        _positions,
+        _settings
+      )
+      when is_number(order_id) do
+    :fetch_buy_order
   end
 
   def generate_decision(
@@ -139,21 +156,6 @@ defmodule Naive.Strategy do
       ) do
     sell_price = calculate_sell_price(buy_price, profit_interval, tick_size)
     {:place_sell_order, sell_price}
-  end
-
-  def generate_decision(
-        %TradeEvent{
-          buyer_order_id: order_id
-        },
-        %Position{
-          buy_order: %Exchange.Order{
-            id: order_id
-          }
-        },
-        _positions,
-        _settings
-      ) do
-    :fetch_buy_order
   end
 
   def generate_decision(
