dd568bba617fb290cf03c404a4bd74bc76b096ff
Kamil Skowron <emanuel1989@gmail.com>
2025-09-01 19:16:52 +0200

    mix format

--- apps/naive/lib/naive/trader.ex
+++ apps/naive/lib/naive/trader.ex
@@ -88,16 +88,16 @@ defmodule Naive.Trader do
         %State{
           id: id,
           symbol: symbol,
-          buy_order:
-            %Binance.OrderResponse{
-              price: buy_price,
-              orig_qty: quantity
-            },
+          buy_order: %Binance.OrderResponse{
+            price: buy_price,
+            orig_qty: quantity
+          },
           sell_order: nil,
           profit_interval: profit_interval,
           tick_size: tick_size
         } = state
-  ) when trade_price < buy_price do
+      )
+      when trade_price < buy_price do
     sell_price = calculate_sell_price(buy_price, profit_interval, tick_size)
 
     Logger.info(
@@ -120,12 +120,12 @@ defmodule Naive.Trader do
         %State{
           id: id,
           symbol: symbol,
-          sell_order:
-            %Binance.OrderResponse{
-              price: sell_price
-            }
+          sell_order: %Binance.OrderResponse{
+            price: sell_price
+          }
         } = state
-  ) when trade_price > sell_price do
+      )
+      when trade_price > sell_price do
     Logger.info("Trader(#{id}) finished trade cycle for #{symbol}")
     {:stop, :normal, state}
   end
