b388d2da9ee181800ab5600f02d27f3f6042ea88
Kamil Skowron <emanuel1989@gmail.com>
2025-09-22 21:08:21 +0200

    Filling orders at the price

--- apps/naive/lib/naive/trader.ex
+++ apps/naive/lib/naive/trader.ex
@@ -67,7 +67,7 @@ defmodule Naive.Trader do
           tick_size: tick_size
         } = state
       )
-      when trade_price < buy_price do
+      when trade_price <= buy_price do
     {:ok, %Binance.Order{} = current_buy_order} =
       Binance.get_order(
         symbol,
@@ -103,7 +103,7 @@ defmodule Naive.Trader do
           }
         } = state
       )
-      when trade_price > sell_price do
+      when trade_price >= sell_price do
     {:ok, %Binance.Order{} = current_sell_order} =
       Binance.get_order(
         symbol,
