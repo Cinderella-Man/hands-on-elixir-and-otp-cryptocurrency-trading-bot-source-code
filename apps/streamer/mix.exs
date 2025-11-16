d46e8a23eeaa0511b4ee00514b45cb99789c5a3d
Kamil Skowron <emanuel1989@gmail.com>
2025-11-16 16:19:35 +0100

    Naive and Streamer apps updated to use the Registry instead of Core application

--- apps/streamer/mix.exs
+++ apps/streamer/mix.exs
@@ -27,7 +27,6 @@ defmodule Streamer.MixProject do
   defp deps do
     [
       {:binance, "~> 1.0"},
-      {:core, in_umbrella: true},
       {:ecto_sql, "~> 3.0"},
       {:ecto_enum, "~> 1.4"},
       {:jason, "~> 1.2"},
