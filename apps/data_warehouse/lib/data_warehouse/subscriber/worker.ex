f85521be0028a5a8e6cf07af7c0d27777ca4051c
Kamil Skowron <emanuel1989@gmail.com>
2025-11-16 15:47:35 +0100

    Update to registry key - upcasing the symbol

--- apps/data_warehouse/lib/data_warehouse/subscriber/worker.ex
+++ apps/data_warehouse/lib/data_warehouse/subscriber/worker.ex
@@ -65,6 +65,6 @@ defmodule DataWarehouse.Subscriber.Worker do
   end
 
   defp via_tuple(topic) do
-    {:via, Registry, {:subscriber_workers, topic}}
+    {:via, Registry, {:subscriber_workers, String.upcase(topic)}}
   end
 end
