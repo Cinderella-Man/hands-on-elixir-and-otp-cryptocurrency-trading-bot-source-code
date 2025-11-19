8e76efda2ed24deb6445dc820b20070abbd8a0a3
Kamil Skowron <emanuel1989@gmail.com>
2025-11-19 22:46:29 +0100

    Comments fixed

--- apps/naive/test/naive_test.exs
+++ apps/naive/test/naive_test.exs
@@ -39,22 +39,28 @@ defmodule NaiveTest do
     # Step 4 - Broadcast 8 events
 
     [
+      # below event will trigger
       # buy order placed @ 0.4307
       generate_event(1, 0.43183010, "213.10000000"),
+      # above the buy price - ignored
       generate_event(2, 0.43183020, "56.10000000"),
+      # above the buy price - ignored
       generate_event(3, 0.43183030, "12.10000000"),
       # event at the expected buy price
+      # it should trigger fetching the buy order
       generate_event(4, 0.4307, "38.92000000"),
       # event below the expected buy price
-      # it should trigger marking buy order as filled
-      # and place sell order @ 0.4319
+      # normally ignored but after fetching the buy order
+      # it should trigger placing a sell order @ 0.4319
       generate_event(5, 0.43065, "126.53000000"),
       # event at exact the expected sell price
+      # it should trigger fetching the sell order
       generate_event(6, 0.4319, "62.92640000"),
-      # event above the expected sell price
-      # it should trigger marking sell order as filled
+      # event after fetching the sell order
+      # causes trader process to exit
       generate_event(7, 0.43205, "345.14235000"),
-      # this one should trigger buy order for a new trader process
+      # below event will trigger
+      # buy order placed @ 0.431
       generate_event(8, 0.43210, "3201.86480000")
     ]
     |> Enum.each(fn event ->
