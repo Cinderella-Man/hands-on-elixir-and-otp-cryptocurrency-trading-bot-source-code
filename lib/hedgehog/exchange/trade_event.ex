77f19549a4d5f7a0ebcff525e0ec959e2004ae87
Kamil Skowron <1019893+Cinderella-Man@users.noreply.github.com>
2026-01-29 14:58:32 +0100

    Gradual typing introduced to TradeEvent, updated websockex library

--- lib/hedgehog/exchange/trade_event.ex
+++ lib/hedgehog/exchange/trade_event.ex
@@ -1,6 +1,20 @@
 defmodule Hedgehog.Exchange.TradeEvent do
   use Ecto.Schema
 
+  @type t :: %__MODULE__{
+          id: Ecto.UUID.t() | nil,
+          event_type: String.t() | nil,
+          event_time: integer() | nil,
+          symbol: String.t() | nil,
+          trade_id: integer() | nil,
+          price: Decimal.t() | nil,
+          quantity: Decimal.t() | nil,
+          trade_time: integer() | nil,
+          buyer_market_maker: boolean() | nil,
+          inserted_at: NaiveDateTime.t() | nil,
+          updated_at: NaiveDateTime.t() | nil
+        }
+
   @primary_key {:id, :binary_id, autogenerate: true}
 
   schema "trade_events" do
--- mix.exs
+++ mix.exs
@@ -75,7 +75,7 @@ defmodule Hedgehog.MixProject do
       {:binance, "~> 1.0"},
       {:decimal, "~> 2.0"},
       {:ecto_enum, "~> 1.4"},
-      {:websockex, "~> 0.4.2"},
+      {:websockex, "~> 0.5"},
       {:mimic, "~> 1.7", only: [:test, :integration]}
     ]
   end
