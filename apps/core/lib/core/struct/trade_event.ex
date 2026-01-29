bf9dce495c53adeb79bc9efd6d66dd165343df53
Kamil Skowron <1019893+Cinderella-Man@users.noreply.github.com>
2026-01-29 14:50:18 +0100

    Gradual typing introduced to TradeEvent, updated websockex library

--- apps/core/lib/core/struct/trade_event.ex
+++ apps/core/lib/core/struct/trade_event.ex
@@ -1,4 +1,15 @@
 defmodule Core.Struct.TradeEvent do
+  @type t :: %__MODULE__{
+          event_type: String.t(),
+          event_time: integer(),
+          symbol: String.t(),
+          trade_id: integer(),
+          price: float(),
+          quantity: String.t(),
+          trade_time: integer(),
+          buyer_market_maker: boolean()
+        }
+
   defstruct [
     :event_type,
     :event_time,
