5178a02b40d184e16d5b6e0c7697b8a7ad45d304
Kamil Skowron <1019893+Cinderella-Man@users.noreply.github.com>
2026-02-02 15:16:17 +0100

    consistency change

--- apps/naive/lib/naive/trader.ex
+++ apps/naive/lib/naive/trader.ex
@@ -25,6 +25,7 @@ defmodule Naive.Trader do
 
   def init(%State{symbol: symbol} = state) do
     symbol = String.upcase(symbol)
+
     Logger.info("Initializing new trader for symbol(#{symbol})")
 
     Phoenix.PubSub.subscribe(
