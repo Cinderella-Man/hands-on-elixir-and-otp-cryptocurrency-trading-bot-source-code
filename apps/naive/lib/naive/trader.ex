a61cbb3db6afbbe2ed9322d4372407067a46c41a
Kamil Skowron <1019893+Cinderella-Man@users.noreply.github.com>
2026-02-02 15:24:08 +0100

    consistency change

--- apps/naive/lib/naive/trader.ex
+++ apps/naive/lib/naive/trader.ex
@@ -44,6 +44,7 @@ defmodule Naive.Trader do
 
   def init(%State{id: id, symbol: symbol} = state) do
     symbol = String.upcase(symbol)
+
     @logger.info("Initializing new trader(#{id}) for #{symbol}")
 
     @pubsub_client.subscribe(
