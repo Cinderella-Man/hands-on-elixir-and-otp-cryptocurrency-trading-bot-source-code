7193a13b004b260e386ce9ae1dfb50893e5a0bca
Kamil Skowron <1019893+Cinderella-Man@users.noreply.github.com>
2026-02-02 20:45:12 +0100

    More tidy up - avoiding elem(1) to not confuse readers

--- apps/naive/lib/naive/strategy.ex
+++ apps/naive/lib/naive/strategy.ex
@@ -432,15 +432,15 @@ defmodule Naive.Strategy do
   end
 
   def fetch_symbol_settings(symbol) do
-    exchange_info = @binance_client.get_exchange_info()
+    {:ok, exchange_info} = @binance_client.get_exchange_info()
     db_settings = @repo.get_by!(Settings, symbol: symbol)
+
     merge_filters_into_settings(exchange_info, db_settings, symbol)
   end
 
   def merge_filters_into_settings(exchange_info, db_settings, symbol) do
     symbol_filters =
       exchange_info
-      |> elem(1)
       |> Map.get(:symbols)
       |> Enum.find(&(&1["symbol"] == symbol))
       |> Map.get("filters")
