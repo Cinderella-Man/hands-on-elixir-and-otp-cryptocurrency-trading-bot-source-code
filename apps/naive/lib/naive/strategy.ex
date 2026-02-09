82dd5f78779db09bdb445b054b110cfe3d1ed3b7
Kamil Skowron <1019893+Cinderella-Man@users.noreply.github.com>
2026-02-09 11:35:08 +0100

    Reverting wrong idea

--- apps/naive/lib/naive/strategy.ex
+++ apps/naive/lib/naive/strategy.ex
@@ -118,15 +118,14 @@ defmodule Naive.Strategy do
         },
         %Position{
           buy_order: %Binance.OrderResponse{
-            price: buy_price,
-            status: status
+            price: buy_price
           },
           sell_order: nil
         },
         _positions,
         _settings
       )
-      when status != "FILLED" and trade_price <= buy_price do
+      when trade_price <= buy_price do
     :fetch_buy_order
   end
 
